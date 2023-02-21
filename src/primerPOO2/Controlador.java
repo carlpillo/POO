abce251906b7565a492792e0e12d3ce89a664e05
Programador <Programador@A1PC6>
2023-02-21 14:00:36 +0100

    añadir controlador

--- src/primerPOO2/Controlador.java
+++ src/primerPOO2/Controlador.java
@@ -2,6 +2,7 @@ package primerPOO2;
 
 import java.util.Scanner;
 
+
 public class Controlador {
 
 	public static void main(String[] args) {
@@ -16,6 +17,7 @@ public class Controlador {
 		System.out.println("cuando cumplas años tendras" + person.edad);
 		System.out.println("cuanto has crecido :");
 		double incremento = new Scanner(System.in).nextDouble();
+//		incremento =Locale.(Locale.US);
 		person.crecer(incremento);
 		System.out.printf("tu estatura ahora es : %.2f",person.estatura);
 	
